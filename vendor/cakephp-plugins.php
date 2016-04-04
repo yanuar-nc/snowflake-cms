7f5c1dd57f7a71caa0adf6330824dad47d5d4267
YanuarNurcahyo <yanuar.nurcahyo6661@gmail.com>
2016-04-04 22:32:41 +0700

    Nambah folder vendor

---
+++ vendor/cakephp-plugins.php
@@ -0,0 +1,12 @@
+<?php
+$baseDir = dirname(dirname(__FILE__));
+return [
+    'plugins' => [
+        'Bake' => $baseDir . '/vendor/cakephp/bake/',
+        'DebugKit' => $baseDir . '/vendor/cakephp/debug_kit/',
+        'Josegonzalez' => $baseDir . '/plugins/Josegonzalez/',
+        'Josegonzalez/Upload' => $baseDir . '/vendor/josegonzalez/cakephp-upload/',
+        'Migrations' => $baseDir . '/vendor/cakephp/migrations/',
+        'Xety/Cake3Upload' => $baseDir . '/vendor/xety/cake3-upload/'
+    ]
+];
